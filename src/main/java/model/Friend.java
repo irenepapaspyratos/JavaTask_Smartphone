a40585c2c41db82ba1a410e588c38c3a2424448f
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-07-05 22:50:10 +0200

    Create public Friend

---
+++ src/main/java/model/Friend.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Friend extends Contact {
+
+    private String phoneNumber;
+
+    public Friend() {
+    }
+    public Friend(String contactName, String phoneNumber) {
+        super(contactName);
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Friend{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", contactName='" + contactName + '\'' +
+                '}';
+    }
+}
